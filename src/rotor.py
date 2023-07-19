aed88097641dc7b23e52c5b89971d2f6c5e80eba
Marthenn <89301265+Marthenn@users.noreply.github.com>
2023-07-19 23:31:16 +0700

    feat: reverse cypher

--- src/rotor.py
+++ src/rotor.py
@@ -47,7 +47,19 @@ class Rotor():
         """
         The reverse cypher of the rotor (left to right in the machine)
         """
-        pass
+        # setup variables
+        char = char.upper()
+        offset = ord(self.position) - ord("A")
+        idx = ord(char) - ord("A")
+
+        mapped = chr((idx+offset)%26 + ord("A"))
+
+        res_idx = (self.wiring.index(mapped) - offset) % 26
+        if res_idx < 0:
+            res_idx += 26
+        res = chr(res_idx + ord("A"))
+
+        return res
 
 class RotorI(Rotor):
     """
@@ -85,46 +97,6 @@ class Reflector(Rotor):
     """
     def __init__(self):
         Rotor.__init__(self)
-        self.wiring = {
-            "A" : "Y",
-            "B" : "R",
-            "C" : "U",
-            "D" : "H",
-            "E" : "Q",
-            "F" : "S",
-            "G" : "L",
-            "H" : "D",
-            "I" : "P",
-            "J" : "X",
-            "K" : "N",
-            "L" : "G",
-            "M" : "O",
-            "N" : "K",
-            "O" : "M",
-            "P" : "I",
-            "Q" : "E",
-            "R" : "B",
-            "S" : "F",
-            "T" : "Z",
-            "U" : "C",
-            "V" : "W",
-            "W" : "V",
-            "X" : "J",
-            "Y" : "A",
-            "Z" : "T"
-        }
+        self.wiring = list("YRUHQSLDPXNGOKMIEBFZCWVJAT")
         self.position = "A"
 
-if __name__ == "__main__":
-    r1 = RotorI()
-    r2 = RotorII()
-    r3 = RotorIII()
-    r1.position = "A"
-    r2.position = "B"
-    r3.position = "A"
-    a3 = r3.forward_cypher("A")
-    a2 = r2.forward_cypher(a3)
-    a1 = r1.forward_cypher(a2)
-    print(a3)
-    print(a2)
-    print(a1)
